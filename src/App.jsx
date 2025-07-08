12cb08d46cb3b4125fe61bf20af25582db67a096
felipin-stack <felipebebesuco@gmail.com>
2025-07-08 20:17:44 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function audio(){
+
+  
+}
+function App() {
+
+   const tocarSom = () => {
+    const audio = new Audio("public/videoplayback (1).mp3"); // Caminho do seu arquivo de som
+    audio.play();
+   }
+  return (
+    <>
+    <h1>DON´T TOUCH THE CAT</h1>
+   <img src='src/images/7qh32v.jpg' alt='meow' onClick={tocarSom}/>
+    </>
+  )
+}
+
+export default App

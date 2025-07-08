efe1bf47065982029eae2066ba37797e1df1449c
felipin-stack <felipebebesuco@gmail.com>
2025-07-08 20:24:18 -0300

    resolvendo caminho de imagem

--- src/App.jsx
+++ src/App.jsx
@@ -10,13 +10,13 @@ function audio(){
 function App() {
 
    const tocarSom = () => {
-    const audio = new Audio("public/videoplayback (1).mp3"); // Caminho do seu arquivo de som
+    const audio = new Audio("/videoplayback (1).mp3"); // Caminho do seu arquivo de som
     audio.play();
    }
   return (
     <>
     <h1>DON´T TOUCH THE CAT</h1>
-   <img src='src/images/7qh32v.jpg' alt='meow' onClick={tocarSom}/>
+   <img src='/7qh32v.jpg' alt='meow' onClick={tocarSom}/>
     </>
   )
 }
